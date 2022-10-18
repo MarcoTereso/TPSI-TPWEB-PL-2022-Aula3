4100ab6cfa1c49cb0f1a240435820cd36d65846a
Marco Tereso <marco.tereso@gmail.com>
2022-10-18 19:42:41 +0100

    estrutura calculadora

---
+++ js/calculadora.js
@@ -0,0 +1,12 @@
+function limpa(){
+	//documento atual + name do form + 
+	//name do input de dados + value para obter o valor
+	document.form1.visor.value = "";
+}
+function valor(v){
+	var atual = document.form1.visor.value;
+	document.form1.visor.value = atual+v;
+}
+function calcula(){
+
+}
